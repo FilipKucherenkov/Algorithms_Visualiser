e3dd9579c6623147eae1eb2c3d77eb0b55894b2a
Filip Kucherenkov <filip.kucherenkov@kcl.ac.uk>
2021-10-30 02:23:44 +0100

    Add error message design

--- visualiser/src/components/errorMsg.jsx
+++ visualiser/src/components/errorMsg.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../styles/error-style.css";
 /**
  * TODO: Add styling and comments
  * @param {*} param0 
@@ -8,7 +8,16 @@ import React from "react";
 const ErrorMsg = ({message}) => {
     console.log(message);
     return (
-        <h1>{message}</h1>
+        <div className="error-container">
+            <div className="cross-container">
+                <img src="red-cross.png" width="30" height="30" className="cross-style"/>
+            </div>
+            <div>
+                <p className="message-style">{message}</p>
+            </div>
+            
+        </div>
+        
     )
 }
 
