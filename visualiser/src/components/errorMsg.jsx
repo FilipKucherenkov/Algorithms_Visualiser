ee02d8cd66e0cf2423eacd5cf1c79deb6416387a
Filip Kucherenkov <filip.kucherenkov@kcl.ac.uk>
2021-10-30 02:25:42 +0100

    Clean errorMsg component and add comments

--- visualiser/src/components/errorMsg.jsx
+++ visualiser/src/components/errorMsg.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import "../styles/error-style.css";
+
 /**
- * TODO: Add styling and comments
- * @param {*} param0 
- * @returns 
+ * Component for custom error message. 
  */
 const ErrorMsg = ({message}) => {
-    console.log(message);
     return (
         <div className="error-container">
             <div className="cross-container">
                 <img src="red-cross.png" width="30" height="30" className="cross-style"/>
             </div>
-            <div>
+            <div className="message-container">
                 <p className="message-style">{message}</p>
-            </div>
-            
-        </div>
-        
+            </div>   
+        </div>     
     )
 }
 
